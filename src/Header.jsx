b5e156edc111aca21b5eb40a29fe68e52146ac42
lakshit1504 <121532634+lakshit1504@users.noreply.github.com>
2023-08-29 00:52:29 +0530

    login button

--- src/Header.jsx
+++ src/Header.jsx
@@ -19,7 +19,7 @@ VacationStay Hub</span>
         <div>Any week</div>
         <div className="border-l border-gray-300"></div>
         <div>Add guests</div>
-        <button className="bg-primary text-white p-1 rounded-full">
+        <button  className="bg-primary text-white p-1 rounded-full">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
           </svg>
@@ -34,7 +34,11 @@ VacationStay Hub</span>
             <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
           </svg>
         </div>
-        {!!user && (
+        {!user?  (
+          <div>
+            login
+          </div>
+        ):  (
           <div>
             {user.name}
           </div>
